f736f366a33f74f195ddfc56cf6a2d008b2d8e1d
realadamsben <88925769+realadamsben@users.noreply.github.com>
2023-04-06 11:07:41 -0400

    Updater 1

--- src/main/java/im/adams/Update.java
+++ src/main/java/im/adams/Update.java
@@ -6,9 +6,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
+import static java.lang.Thread.sleep;
+
 public class Update {
-    public static void main(String[] args) throws IOException {
-        //
+    public static void main(String[] args) throws IOException, InterruptedException {
+        sleep(1000);
         Files.copy(Path.of("CoolBot" + args[0] + ".jar"), Path.of("CoolBot.jar"), StandardCopyOption.REPLACE_EXISTING);
         Process process = Runtime.getRuntime().exec("java -jar CoolBot.jar");
     }
