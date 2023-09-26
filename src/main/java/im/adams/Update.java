84e67dac3c1669807a56031d6918e032a30d1345
realadamsben <88925769+realadamsben@users.noreply.github.com>
2023-09-26 10:37:23 -0400

    Reduce sleep time on updater

--- src/main/java/im/adams/Update.java
+++ src/main/java/im/adams/Update.java
@@ -10,8 +10,9 @@ import static java.lang.Thread.sleep;
 
 public class Update {
     public static void main(String[] args) throws IOException, InterruptedException {
-        sleep(1000);
+        sleep(10);
         Files.copy(Path.of("CoolBot" + args[0] + ".jar"), Path.of("CoolBot.jar"), StandardCopyOption.REPLACE_EXISTING);
         Process process = Runtime.getRuntime().exec("java -jar CoolBot.jar");
+        Files.delete(Path.of("CoolBot" + args[0] + ".jar"));
     }
 }
