3df03abe27c07793a597fda9ffc2ba58f625159d
realadamsben <88925769+realadamsben@users.noreply.github.com>
2023-10-06 21:21:36 -0400

    Update Update.java

--- src/main/java/im/adams/Update.java
+++ src/main/java/im/adams/Update.java
@@ -10,7 +10,7 @@ import static java.lang.Thread.sleep;
 
 public class Update {
     public static void main(String[] args) throws IOException, InterruptedException {
-        sleep(10);
+        sleep(500);
         Files.copy(Path.of("CoolBot" + args[0] + ".jar"), Path.of("CoolBot.jar"), StandardCopyOption.REPLACE_EXISTING);
         Runtime.getRuntime().exec("java -jar CoolBot.jar");
         sleep(10000);
